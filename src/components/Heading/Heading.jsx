bf00f7ec3d74531b94532586572d1cc3b876aac8
FounDy <vkadgcundgd@gmail.com>
2025-04-01 14:03:45 +0200

    2_lecture

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,6 +1,12 @@
-const Heading = ({ text, tag = "h2" }) => {
+import s from "./Heading.module.css"
+import clsx from 'clsx';
+
+const Heading = ({ text, tag = "h2" ,bottom, mTop}) => {
   const Tag = tag;
-  return <Tag>{text}</Tag>;
+  return <Tag className={clsx(s.title ,{[s.bottom ]: bottom,[s.top]: mTop})}>{text}</Tag>;
 };
 
 export default Heading;
+// [s.title,bottom?s.bottom :""].join(" ")
+// clsx(s.title ,bottom&&s.bottom, mTop && s.top)
+// clsx(s.title ,{[s.bottom ]: bottom,[s.top]: mTop})
